72d01580536d1517ffb2d9c79248a53a388afce5
李财宝 <1825306263@qq.com>
2019-01-15 19:51:39 +0800

    first init

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 
 int main(){
+   int qian=0;
+   while(1){
+       printf("1---cunqian\n");
+        printf("2---quqian\n");
+        printf("3---yue\n");
+        printf("4---tuichu\n");
 
-    printf("hello world!");
+        printf("gongneng\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+
+
+   }
 
     return 0;
 
